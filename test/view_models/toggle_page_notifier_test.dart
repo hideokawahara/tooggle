ae51294fb01ca18f8d4de14d0ca7225d7b9a08c4
hideokawahara <kawahide.72@gmail.com>
2023-05-18 08:34:31 +0900

    feat: トグルの振動テスト追加

--- test/view_models/toggle_page_notifier_test.dart
+++ test/view_models/toggle_page_notifier_test.dart
@@ -4,9 +4,8 @@ import 'package:tooggle/view_models/view_models_export.dart';
 import 'package:tooggle/models/models_export.dart';
 
 void main() {
-  late final StateNotifierProvider<TogglePageNotifier, TogglePageState>
-      provider;
-  late final TogglePageNotifier notifier;
+  late StateNotifierProvider<TogglePageNotifier, TogglePageState> provider;
+  late TogglePageNotifier notifier;
   late TogglePageState Function() getState;
   setUp(() {
     provider = StateNotifierProvider<TogglePageNotifier, TogglePageState>(
@@ -23,6 +22,34 @@ void main() {
       notifier.changeIsOnStatus(true);
       expect(notifier.debugState.isOn, true);
       expect(getState().isOn, true);
+    });
+  });
+  group('トグルの振動の確認のテスト', () {
+    test('振動のデフォルトのテスト', () {
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.weak);
+      expect(getState().selectFeedBack, TapFeedBack.weak);
+      notifier.changeFeedBack(TapFeedBack.medium);
+    });
+    test('振動を中に変更テスト', () {
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.weak);
+      expect(getState().selectFeedBack, TapFeedBack.weak);
+      notifier.changeFeedBack(TapFeedBack.medium);
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.medium);
+      expect(getState().selectFeedBack, TapFeedBack.medium);
+    });
+    test('振動を強に変更テスト', () {
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.weak);
+      expect(getState().selectFeedBack, TapFeedBack.weak);
+      notifier.changeFeedBack(TapFeedBack.strong);
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.strong);
+      expect(getState().selectFeedBack, TapFeedBack.strong);
+    });
+    test('振動をバイブに変更テスト', () {
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.weak);
+      expect(getState().selectFeedBack, TapFeedBack.weak);
+      notifier.changeFeedBack(TapFeedBack.vibe);
+      expect(notifier.debugState.selectFeedBack, TapFeedBack.vibe);
+      expect(getState().selectFeedBack, TapFeedBack.vibe);
     });
   });
 }
