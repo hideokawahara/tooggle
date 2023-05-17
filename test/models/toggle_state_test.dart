2d1f741f3623b27e61f5f66cd1593e3365e1a27b
hideokawahara <kawahide.72@gmail.com>
2023-05-18 08:51:14 +0900

    feat: トグルモデルのテスト作成

---
+++ test/models/toggle_state_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tooggle/packages/packages_export.dart';
+import 'package:tooggle/models/models_export.dart';
+
+void main() {
+  late TogglePageState toggle;
+  setUp(() {
+    toggle = const TogglePageState();
+  });
+  test('トグルモデルのデフォルトの確認テスト', () {
+    expect(toggle.isOn, false);
+    expect(toggle.selectFeedBack, TapFeedBack.weak);
+    expect(toggle.selectColor, ToggleColor.green);
+    expect(toggle.toggleSize, 200);
+    expect(toggle.popUpStatus, false);
+    expect(toggle.popupText, 'オフにしてもいいでしょうか？');
+    expect(toggle.position, const Offset(0, 0));
+  });
+}
